5f4cb1ff0175c90a918f0e35b72ce51d2867af08
Stefan Micheelsen <stufkan@gmail.com>
2015-03-26 21:56:28 +0100

    implementation of a simple webrequest for issuing commands to foobar #3

---
+++ app/src/main/java/dk/stufkan/foobarcontrolwidget/library/FoobarHttpControl.java
@@ -0,0 +1,36 @@
+package dk.stufkan.foobarcontrolwidget.library;
+
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+
+/**
+ * Created by Stefan Micheelsen on 26-03-2015.
+ */
+public class FoobarHttpControl {
+
+    private void webrequest(String suffix)
+    {
+        String prefix = "192.168.1.4:8888";
+        String url = prefix + suffix;
+
+        HttpClient httpclient = new DefaultHttpClient();
+
+        // Prepare a request object
+        HttpGet httpget = new HttpGet(url);
+
+        try {
+            HttpResponse response = httpclient.execute(httpget);
+        }
+        catch(IOException e)
+        {
+            Log.d("foobarhttpcontrol", "Could not connect to " + prefix + "\nPlease check the IP and port and ensure that the host is able to accept connections");
+        }
+    }
+}
