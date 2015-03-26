b9a4c38d16d3d2c484d8b3ae97ad4cc519dd759e
Stefan Micheelsen <stufkan@gmail.com>
2015-03-26 22:05:59 +0100

    implementet individual foobar control methods

--- app/src/main/java/dk/stufkan/foobarcontrolwidget/library/FoobarHttpControl.java
+++ app/src/main/java/dk/stufkan/foobarcontrolwidget/library/FoobarHttpControl.java
@@ -3,7 +3,6 @@ package dk.stufkan.foobarcontrolwidget.library;
 import android.util.Log;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -11,9 +10,13 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import java.io.IOException;
 
 public class FoobarHttpControl {
+    private String prefix;
 
-    private void webrequest(String suffix)
-    {
+    public FoobarHttpControl(String prefix) {
+        this.prefix = prefix;
+    }
+
+    private void webrequest(String suffix) {
         String prefix = "192.168.1.4:8888";
         String url = prefix + suffix;
 
@@ -24,10 +27,28 @@ public class FoobarHttpControl {
 
         try {
             HttpResponse response = httpclient.execute(httpget);
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             Log.d("foobarhttpcontrol", "Could not connect to " + prefix + "\nPlease check the IP and port and ensure that the host is able to accept connections");
         }
+    }
+
+    public void playPause() {
+        webrequest("?cmd=PlayOrPause&param1=");
+    }
+
+    public void random() {
+        webrequest("?cmd=StartRandom&param1=");
+    }
+
+    public void next() {
+        webrequest("?cmd=StartNext&param1=");
+    }
+
+    public void mute() {
+        webrequest("?cmd=VolumeMuteToggle");
+    }
+
+    public void volume(int value) {
+        webrequest("?cmd=Volume&param1=" + value);
     }
 }
