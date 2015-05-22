f314d2f7ac2097c717f86a0abfee0e61caa748f8
Stefan Micheelsen <stufkan@gmail.com>
2015-05-22 22:04:14 +0200

    implement FoobarHttpControl as a Broadcastreceiver

--- app/src/main/java/dk/stufkan/foobarcontrolwidget/library/FoobarHttpControl.java
+++ app/src/main/java/dk/stufkan/foobarcontrolwidget/library/FoobarHttpControl.java
@@ -1,7 +1,12 @@
 package dk.stufkan.foobarcontrolwidget.library;
 
+import android.app.IntentService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
 
 import org.apache.http.HttpResponse;
@@ -12,21 +17,14 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.IOException;
 
-public class FoobarHttpControl {
+public class FoobarHttpControl extends BroadcastReceiver {
+    String TAG = "FoobarHttpControl";
+
     private String prefix;
     ControlWebRequest control;
 
-    public FoobarHttpControl(String prefix) {
-        this.prefix = prefix;
-        control = new ControlWebRequest(prefix);
-
-        Log.d("foobarhttpcontrol", "constructor");
-    }
-
-
-
     public void playPause() {
-
+        Log.d(TAG,"playPause");
         control.execute("?cmd=PlayOrPause&param1=");
     }
 
@@ -44,5 +42,21 @@ public class FoobarHttpControl {
 
     public void volume(int value) {
         control.execute("?cmd=Volume&param1=" + value);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive");
+        Bundle extras = intent.getExtras();
+
+        String action = extras.getString("action");
+        String prefix = extras.getString("prefix");
+
+        control = new ControlWebRequest(prefix);
+
+        Log.d(TAG, "Indhold " + action + " " + prefix);
+        if (action.equals("playPause")) {
+            playPause();
+        }
     }
 }
